d2f7aa165f0bd024c34cdf1ade7b5643087a29ac
khcheung <alexcheung1980@hotmail.com>
2026-06-22 13:54:50 +0000

    Update Add Key Attribute

--- WebbSite.Common/Models/DbModels/ModelBase.cs
+++ WebbSite.Common/Models/DbModels/ModelBase.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebbSite.Common.Models.DbModels;
 public abstract class ModelBase
 {
+    [Key]
     [Column("id")]
     public Int64 Id { get; set; }
 }
